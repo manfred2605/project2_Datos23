cec1273f3f90efafd3778764a0e7de4702a05ad1
manfred2605 <80372230+manfred2605@users.noreply.github.com>
2023-11-12 11:06:11 -0600

    completan variables

--- VariableTypes.py
+++ VariableTypes.py
@@ -1,6 +1,6 @@
 class VariableTypes:
-    def __init__(self, value="", _id="", name="", _type="", scope=""):
-        self.value = value
+    def __init__(self, _value="", _id="", name="", _type="", scope=""):
+        self.value = _value
         self.id = _id
         self.name = name
         self.type = _type
@@ -13,3 +13,35 @@ class VariableTypes:
     @value.setter
     def value(self, value):
         self.value = value
+
+    @property
+    def id(self):
+        return self.id
+
+    @id.setter
+    def id(self, value):
+        self._id = value
+
+    @property
+    def name(self):
+        return self.name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def type(self):
+        return self.type
+
+    @type.setter
+    def type(self, value):
+        self._type = value
+
+    @property
+    def scope(self):
+        return self.scope
+
+    @scope.setter
+    def scope(self, value):
+        self._scope = value
