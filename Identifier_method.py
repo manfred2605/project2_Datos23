9bd554f0ca547326880e89b82bc96dad5fccc857
manfred2605 <80372230+manfred2605@users.noreply.github.com>
2023-11-12 17:01:53 -0600

    metodo read_file para insercion en tabla(lista), verficar si esta bien.

---
+++ Identifier_method.py
@@ -0,0 +1,37 @@
+import VariableTypes
+
+
+def __equal_brackets_count(text):
+    pass
+
+
+class Identifier_method:
+    def __init__(self):
+        self.__current_line = None
+        self.hash_table = {}
+
+    def read_file(self, path):
+
+        file = open(path, "r", encoding="utf-8")  # abre el archivo de texto con codificacion UTF-8
+        hash_table = file.readlines()             # lea las lineas y las guarda en una lista hash_table
+        file.seek(0)                        # se coloca al inico del archivo para siempre leerlo desde el principio
+        text = file.read().strip()
+        file.close()
+        for line in hash_table:
+            self.__current_line += 1
+            new_line = line.strip()
+            if new_line == "":
+                continue
+            if new_line[-1] != "{" and new_line[-1] != "}" and new_line[-1] != "(" and new_line[-1] != ";":
+                new_line += ";"
+            self.__process_line(
+                new_line, self.__equal_brackets_count(text))
+
+    def __equal_brackets_count(self, text):
+        """Verifica que la cantidad de llaves de apertura se la misma que las de cierre
+      
+               """
+        return text.count("{") - text.count("}") == 0
+
+    def __process_line(self, new_line, param):
+        pass
