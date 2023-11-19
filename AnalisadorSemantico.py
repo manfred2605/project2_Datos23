134d9c14f23a48c89d8e11c331779efb91db32b2
manfred2605 <80372230+manfred2605@users.noreply.github.com>
2023-11-19 07:46:12 -0600

    metodo analizar_linea

--- AnalisadorSemantico.py
+++ AnalisadorSemantico.py
@@ -1,4 +1,4 @@
-# import TablaSimbolos as TB
+import TablaSimbolos as TB
 
 arit_ops = ["+", "-", "*", "**", "/", "//", "%"]
 comp_ops = [">", "<", ">=", "<=", "==", "!="]
@@ -7,13 +7,20 @@ tipos_validos = ["int", "float", "string"]
 
 
 class AnalizadorSemantico:
-    """
-    _summary_
-    """
 
     def __init__(self):
         """
         Constructor
         """
-        #self.tabla_simbolos = TB.TablaSimbolos()
+        self.tabla_simbolos = TB.TablaSimbolos()
         self.errores = []
+
+    def analizar_llamada(self, tokens, num_linea):
+        self.tabla_simbolos.insertar(
+            "statement", {"tipo": "call", "valor": tokens[0], "linea": num_linea}
+        )
+
+        if self.analizar_linea(tokens[1:], num_linea) != None:
+            self.errores.append(
+                f"Error – Línea {num_linea}: Argumentos inválidos en llamada {tokens[0]}."
+            )
