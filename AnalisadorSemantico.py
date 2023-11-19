1242878481fe4aab888e07ba32889fe52cc51f30
SleepinGiank <giank.ch.m7645@gmail.com>
2023-11-19 10:54:04 -0600

    Creado metodo analizar_condicion

--- AnalisadorSemantico.py
+++ AnalisadorSemantico.py
@@ -32,6 +32,116 @@ class AnalizadorSemantico:
         else:
             for error in self.errores:
                 print(error)
+    
+    
+    def analizar_condicion(self, tokens_condicion, num_linea):
+        """
+        Funcion para analizar una condicion dentro de linea de codigo
+
+        Args:
+            tokens (_type_): _description_
+            num_linea (_type_): _description_
+            :param num_linea:
+            :param tokens_condicion:
+        """
+        tipo_actual = None
+
+        for token in tokens_condicion:
+            if token.isalnum():
+                if self.validar_identificador(token) == "string":
+                    if self.tabla_simbolos.buscar(token):
+                        pass
+                    else:
+                        self.tabla_simbolos.insertar(
+                            token,
+                            {"tipo": "string", "valor": f"{token}", "linea": num_linea},
+                        )
+
+                if self.validar_identificador(token) == "int":
+                    if self.tabla_simbolos.buscar(token):
+                        pass
+                    else:
+                        self.tabla_simbolos.insertar(
+                            token, {"tipo": "int", "valor": token, "linea": num_linea}
+                        )
+
+                if self.validar_identificador(token) == "float":
+                    if self.tabla_simbolos.buscar(token):
+                        pass
+                    else:
+                        self.tabla_simbolos.insertar(
+                            token, {"tipo": "float", "valor": token, "linea": num_linea}
+                        )
+
+                elif self.validar_identificador(token) == None:
+                    self.errores.append(
+                        f"Error – Línea {num_linea}: '{token}' no es argumento válido."
+                    )
+                    return
+
+                # Verifica si el identificador ya esta declarado
+                if not self.tabla_simbolos.buscar(token):
+                    self.errores.append(
+                        f"Error – Línea {num_linea}: '{token}' no está declarado."
+                    )
+                    return
+                else:
+                    # Obtiene el tipo del identificador de la tabla de simbolos
+                    tipo_identificador = self.tabla_simbolos.buscar(token)["tipo"]
+
+                if tipo_actual is None:
+                    tipo_actual = tipo_identificador
+
+                # Realiza verificacion de tipos
+                elif tipo_actual != tipo_identificador:
+                    self.errores.append(
+                        f"Error – Línea {num_linea}: Tipos incompatibles en la condición."
+                    )
+                    return
+
+            if token in comp_ops:
+                # Se verifica si se utilizan operadores de comparacion
+                if not self.tabla_simbolos.buscar(token):
+                    self.tabla_simbolos.insertar(
+                        token, {"tipo operador": "comparacion", "linea": num_linea}
+                    )
+
+                if tipo_actual not in tipos_validos:
+                    self.errores.append(
+                        f"Error – Línea {num_linea}: Operador de comparación '{token}' no válido para el tipo de dato."
+                    )
+                    return
+
+            if token in arit_ops:
+                # Se verifica si se utilizan operadores aritmeticos
+                if not self.tabla_simbolos.buscar(token):
+                    self.tabla_simbolos.insertar(
+                        token, {"tipo operador": "aritmetico", "linea": num_linea}
+                    )
+
+                if tipo_actual not in ["int", "float"]:
+                    self.errores.append(
+                        f"Error – Línea {num_linea}: Operador aritmético '{token}' no válido para el tipo de dato."
+                    )
+                    return
+
+            if token in logic_ops:
+                # Se verifica si se utilizan operadores logicos
+                if not self.tabla_simbolos.buscar(token):
+                    self.tabla_simbolos.insertar(
+                        token, {"tipo operador": "logico", "linea": num_linea}
+                    )
+            else:
+                # Se verifican otros casos de ser necesarios
+                pass
+
+        # Verifica si el tipo es valido en la condicion
+        if tipo_actual not in tipos_validos:
+            self.errores.append(
+                f"Error – Línea {num_linea}: Tipo no permitido en la condición."
+            )
+        else:
+            pass
 
     def analizar_llamada(self, tokens, line_number):
         self.tabla_simbolos.insertar(
